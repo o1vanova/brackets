e7432202d315638ee383c79ce333d31231804592
Olga Ivanova <olgai1409@gmail.com>
2019-09-23 17:06:14 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let dictionary = [];
+  
+  bracketsConfig.forEach(x => {
+    dictionary[x[0]] = x[1];
+  });
+  
+  let result = str.split('').reduce((story, current) => {
+    let lastElement = story[story.length - 1];
+    
+    if(dictionary[lastElement] === current) {
+      story.pop();
+    } else {
+      story.push(current);
+    }
+
+    return story;    
+  }, []);
+
+  return !result.length;
 }
